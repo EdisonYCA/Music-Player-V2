eff419587e83f0a2fe5973e1b87cd9c9594829b5
EdisonYCA <edisonyca127@gmail.com>
2022-11-23 23:27:55 -0500

    rename to 'client_ui.' create function to hand client requests

--- MusicPlayerDB/src/client_ui.py
+++ MusicPlayerDB/src/client_ui.py
@@ -1,9 +1,25 @@
 from music_player_v1_package.src import *
 
 
-def main():
-    artist_ui.ui()
+# client interface
+def ui():
+    pass
+    # client can search for artist, song, or album
+
+
+def searched_song(song_searched):
+    """Client has searched for a song"""
+    pass
+
+
+def searched_album(album_searched):
+    """Client has searched for an album"""
+    pass
+
+
+def searched_artist(artist_searched):
+    """Client has searched for an artist"""
 
 
 if __name__ == "__main__":
-    main()
+    ui()
