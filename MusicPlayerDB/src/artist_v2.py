f28b2adfd1453e212c8a98b91cccdb0df0502f64
EdisonYCA <edisonyca127@gmail.com>
2022-11-30 09:40:11 -0500

    Update get_albums method to fetch artist albums in database. Update get_songs method to fetch artists songs in database.

--- MusicPlayerDB/src/artist_v2.py
+++ MusicPlayerDB/src/artist_v2.py
@@ -1,67 +1,62 @@
 import database_handler as db
+import os
 
 
 class Artist:
     def __init__(self, name):
         self.name = name
         db.store_artist(self.name)  # store artist in database
-        self.information = {}  # empty dictionary containing of the information on the artist
+        # dictionary containing of the information on the artist
+        self.information = {"database_location": db.locate_artist(self.name),
+                            "Albums": self.get_albums(),
+                            "Songs": self.get_songs()}
 
     def get_albums(self):
-        """Returns a list of artists albums"""
-        return self.albums
+        """Returns an artists albums with song count"""
+        # Locate artist in database
+        albums_location = os.path.join(db.locate_artist(self.name), 'Albums')
 
-    def print_albums(self):
-        """Returns a string of artists albums"""
-        album_names = "None"
-        if self.albums is not None:
-            album_names = ""
-            for album in range(len(self.albums)):
-                album_names += str(self.albums[album])
-                album_names += "\t\t"
-            return album_names
-        return album_names
+        # Iterate through album names and print them
+        song_count = 0
+        for albums in os.listdir(albums_location):
+            album_name = albums
+            for songs in os.listdir(os.path.join(albums_location, albums)):
+                song_count += 1
 
-    def print_songs(self):
-        """returns a string of artists songs"""
-        song_names = "None"
-        if self.songs is not None:
-            song_names = ""
-            for song in range(len(self.songs)):
-                song_names += str(self.songs[song])
-                song_names += "\t"
-            return song_names
-        return song_names
+            # format output
+            if song_count == 1:
+                print(f"{album_name.title()}: {song_count} song")
+                song_count = 0
+            else:
+                print(f"{album_name.title()}: {song_count} songs")
+                song_count = 0
+
+    def get_songs(self):
+        """Returns an artists songs"""
+        # Locate artist in database
+        songs_location = os.path.join(db.locate_artist(self.name), 'Songs')
+
+        song_names = []
+        # Iterate through albums
+        for songs in os.listdir(songs_location):
+            format_song_name = songs.split(".txt")
+            song_names.append(format_song_name[0].title())
+
+        print(*song_names, sep=", ")
 
     def get_name(self):
         """Returns artists name"""
         return self.name.title()
 
-    def get_songs(self):
-        """Returns an artists songs"""
-        return self.songs
-
     def add_album(self, album):
-        """add album to artists current album list"""
-        for song in range(len(album.songs)):  # all songs in album should also be added into the artists song list
-            self.add_song(album.songs[song])
-
-        if self.get_albums() is None:
-            self.albums = []
-            self.albums.append(album)
-        else:
-            self.albums.append(album)
+        """add album to artists current album storage"""
+        db.store_album(self.name, album)
 
     def add_song(self, song):
-        """add song to artists current song list"""
-        if self.get_songs() is None:
-            self.songs = []
-            self.songs.append(song)
-        else:
-            self.songs.append(song)
+        """add song to artists current song storage"""
+        db.store_song(self.name, song)
 
-    def __repr__(self):
-        """Represent artist object as a string"""
-        return f"Artist: {self.name.title()}\n" \
-               f"Albums: {str(self.print_albums())}\n" \
-               f"Songs: {str(self.print_songs())}"
+    # def __repr__(self):
+    #     """Represent artist object as a string"""
+    #     return f"Artist: {self.name.title()}\n" \
+    #            f"Albums: {self.get_albums()}\n"
