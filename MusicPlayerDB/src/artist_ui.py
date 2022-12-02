dd6ca9680e7b89201c77b621f9143dca2129beb4
EdisonYCA <edisonyca127@gmail.com>
2022-12-02 08:05:42 -0500

    Modify ui to allow existing artists to add songs and albums to their profile

--- MusicPlayerDB/src/artist_ui.py
+++ MusicPlayerDB/src/artist_ui.py
@@ -1,15 +1,23 @@
 from artist import Artist
-from album import Album
-from song import Song
+import database_handler as db
 
 
 def ui():
     run_program = True
+    new_user = main()
 
     while run_program:
         # ask the user to enter their artist name
-        print("Let's get your profile set up on our Artist database!")
         artist_name = input("Enter your artist name -> ")
+
+        if not db.locate_artist(artist_name) and new_user == "2":
+            print(f"I'm sorry, we weren't able to locate '{artist_name}' in our database.\n")
+            exit()
+        else:
+            artist = artist_name
+
+        if new_user == "1":
+            artist = Artist(artist_name).get_name()
 
         # ask the artist to enter a song or album
         print(f"\n{artist_name}, would you like to enter a song, or a album to your profile?\n"
@@ -18,21 +26,35 @@ def ui():
               "3. None")
         request = input("-> ")
 
-        artist = Artist(artist_name)
         if request == "1":
             song_request(artist)
-            print("\nGreat! Here is an overview of your new profile:")
+            print("\nGreat! Here is an overview of your profile: ")
             print(artist)
 
         elif request.lower() == "2":
             album_request(artist)
+            print(f"Success")
+            print("Here is an overview of your profile: \n")
             print(artist)
 
         else:
-            print(f"Okay {artist_name}, you're now stored in our database. Thank you!")
+            print("Success")
+            print("Here is an overview of your profile: \n")
             print(artist)
 
         run_program = False
+
+
+def main():
+    print("Would you like to add a song/album to an existing profile, or create a new profile?"
+          "\n1. Create new profile"
+          "\n2. Existing profile")
+    new = input("-> ")
+
+    if new == "1":
+        print("Let's get your profile set up on our Artist database!")
+
+    return new
 
 
 def album_request(artist):
@@ -43,7 +65,7 @@ def album_request(artist):
         print(f"\n{album_name} will at-least two songs.")
         songs = song_request(artist)
 
-        artist.add_album(album_name, songs)
+        db.store_album(artist, album_name, songs)
         albums.append(album_name)
         print("Would you like to add another album?\n"
               "1. Yes\n"
@@ -51,9 +73,7 @@ def album_request(artist):
         add_album = input("-> ")
         if add_album == "2":
             break
-    print(f"Okay {artist.get_name()}, ", end="")
-    print(*albums, sep=", ", end=" ")
-    print("are now on your profile!")
+    print("Successfully added album.\n")
 
 
 def song_request(artist):
@@ -61,10 +81,9 @@ def song_request(artist):
     while True:
         print("Enter the name of the song")
         song_name = input("-> ")
-        song = song_name + ".txt"
 
-        artist.add_song(song)
-        songs.append(song)
+        db.store_song(artist, song_name)
+        songs.append(song_name)
 
         print("Would you like to add another song?\n"
               "1. Yes\n"
